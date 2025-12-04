1dd39aa80f6eeccce2c8c47e724d9f46303e3063
Sandeep Tanjore <xyz@gmail.com>
2025-12-03 21:33:04 -0500

    Remove exercise files 29-41 from wrong location

--- 01 - Language Basics/38-exercise.js
+++
@@ -1,6 +0,0 @@
-/**
- * Exercise 38 - Which is the biggest?
- * Given the numbers 1,8,2,9,3,7,4,6, use JavaScript to determine which is the largest number.
- */
-
-console.log('The maximum from the given set of numbers is: ' + Math.max(1, 8, 2, 9, 3, 7, 4, 6));
--- 01 - Language Basics/40-exercise.js
+++
@@ -1,8 +0,0 @@
-/**
- * Exercise 40 - No dice
- * Create a random number with JavaScript that represents a value found on a
- * common set of playing dice (1-6). Log the value to the console to check the value you have generated.
- */
-
-let diceNumber = Math.floor(Math.random() *6) +1;
-console.log(`Random number generated between 1 and 6 is: ${diceNumber}`);
